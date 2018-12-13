06b3f7814378c9b5b4dd16963e72be450c91bceb
Lance Hamilton <lance.hamilton24@gmail.com>
2018-12-12 20:45:10 -0600

    added github authentication

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Blogs.scss';
+
+class Blogs extends React.Component {
+  render() {
+    return (
+      <div className="blogs">
+        <h2>Blogs</h2>
+      </div>
+    );
+  }
+}
+
+export default Blogs;
--- src/helpers/data/authRequests.js
+++ src/helpers/data/authRequests.js
@@ -2,7 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const authenticate = () => {
-
-};
+  const provider = new firebase.auth.GithubAuthProvider();
+  firebase.auth().signInWithRedirect(provider);
+}
 
 export default { authenticate };
